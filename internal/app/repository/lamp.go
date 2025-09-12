ca69c2a492beb32fe46b8157487de46bfa4d11e1
DeveloperMan313 <053000voloshch@gmail.com>
2025-09-12 18:18:28 +0300

    Add MinIO/nginx configs, add 12 lamps, edit card title display and fix page sizing

--- internal/app/repository/lamp.go
+++ internal/app/repository/lamp.go
@@ -22,54 +22,18 @@ type Lamp struct {
 }
 
 var lamps = []Lamp{
-	{
-		ID:                 1,
-		Title:              "Умная люстра",
-		PowerW:             100,
-		LuminousFluxLm:     1000,
-		ScatteringAngleDeg: 100,
-		ImageURL:           "https://placehold.co/300",
-	},
-	{
-		ID:                 2,
-		Title:              "Slim Magnetic",
-		PowerW:             100,
-		LuminousFluxLm:     1000,
-		ScatteringAngleDeg: 100,
-		ImageURL:           "https://placehold.co/300",
-	},
-	{
-		ID:                 3,
-		Title:              "Светильник потолочный",
-		PowerW:             100,
-		LuminousFluxLm:     1000,
-		ScatteringAngleDeg: 100,
-		ImageURL:           "https://placehold.co/300",
-	},
-	{
-		ID:                 4,
-		Title:              "Потолочный светильник",
-		PowerW:             100,
-		LuminousFluxLm:     1000,
-		ScatteringAngleDeg: 100,
-		ImageURL:           "https://placehold.co/300",
-	},
-	{
-		ID:                 5,
-		Title:              "Подвесной светильник",
-		PowerW:             100,
-		LuminousFluxLm:     1000,
-		ScatteringAngleDeg: 100,
-		ImageURL:           "https://placehold.co/300",
-	},
-	{
-		ID:                 6,
-		Title:              "Esthetic Magnetic",
-		PowerW:             100,
-		LuminousFluxLm:     1000,
-		ScatteringAngleDeg: 100,
-		ImageURL:           "https://placehold.co/300",
-	},
+	{ID: 1, Title: "Умная потолочная люстра с перламутром", PowerW: 40, LuminousFluxLm: 3200, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/1.jpg"},
+	{ID: 2, Title: "Slim Magnetic Трековый светильник 26W 4000K Most чёрный", PowerW: 26, LuminousFluxLm: 2210, ScatteringAngleDeg: 60, ImageURL: "http://localhost:9000/lamp-images/2.jpg"},
+	{ID: 3, Title: "Светильник потолочный светодиодный Trio 8W 3000K белый", PowerW: 8, LuminousFluxLm: 680, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/3.jpg"},
+	{ID: 4, Title: "Потолочный светильник", PowerW: 40, LuminousFluxLm: 3200, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/4.jpg"},
+	{ID: 5, Title: "Подвесной светильник со стеклянными плафонами", PowerW: 40, LuminousFluxLm: 3200, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/5.jpg"},
+	{ID: 6, Title: "Esthetic Magnetic Трековый светильник 3W 3000K (чёрный)", PowerW: 3, LuminousFluxLm: 255, ScatteringAngleDeg: 60, ImageURL: "http://localhost:9000/lamp-images/6.jpg"},
+	{ID: 7, Title: "Подвесной светильник", PowerW: 40, LuminousFluxLm: 3200, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/7.jpg"},
+	{ID: 8, Title: "Трековый светильник 100W 4200K Full Light N05 Slim Magnetic", PowerW: 100, LuminousFluxLm: 8500, ScatteringAngleDeg: 60, ImageURL: "http://localhost:9000/lamp-images/8.jpg"},
+	{ID: 9, Title: "Светильник встраиваемый светодиодный Forte 15W 4000K титан", PowerW: 15, LuminousFluxLm: 1275, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/9.jpg"},
+	{ID: 10, Title: "Подвесной светильник со стеклянными плафонами", PowerW: 40, LuminousFluxLm: 3200, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/10.jpg"},
+	{ID: 11, Title: "Светильник потолочный светодиодный Tend 9W 4000K черный", PowerW: 9, LuminousFluxLm: 765, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/11.jpg"},
+	{ID: 12, Title: "Подвесной светодиодный светильник", PowerW: 40, LuminousFluxLm: 3200, ScatteringAngleDeg: 120, ImageURL: "http://localhost:9000/lamp-images/12.jpg"},
 }
 
 func (*LampRepository) GetLampByID(id int) (*Lamp, error) {
