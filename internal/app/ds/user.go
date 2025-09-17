f4414dc8e4747525f62b125c0193ee725f737473
DeveloperMan313 <053000voloshch@gmail.com>
2025-09-17 11:14:53 +0300

    Simplify User model

--- internal/app/ds/user.go
+++ internal/app/ds/user.go
@@ -3,7 +3,6 @@ package ds
 type User struct {
 	ID       uint64 `gorm:"primaryKey"`
 	Username string `gorm:"type:varchar(50);not null;unique"`
-	PassHash string `gorm:"type:varchar(64);not null"`
-	PassSalt string `gorm:"type:varchar(64);not null"`
+	Passwrd  string `gorm:"type:varchar(50);not null"`
 	IsMod    bool   `gorm:"type:boolean;not null"`
 }
