a006c48d68f2475d9634d557f6b80231870f6b4a
DeveloperMan313 <053000voloshch@gmail.com>
2025-11-12 15:04:20 +0300

    Return id/user_id with light request

--- internal/app/ds/light_request.go
+++ internal/app/ds/light_request.go
@@ -3,9 +3,9 @@ package ds
 import "time"
 
 type LightRequest struct {
-	ID                 uint64               `gorm:"primaryKey" json:"-"`
+	ID                 uint64               `gorm:"primaryKey" json:"id"`
 	Status             uint8                `gorm:"not null;default:1" json:"status"` // 1 - draft, 2 - deleted, 3 - pending, 4 - resolved, 5 - rejected
-	UserID             uint64               `gorm:"not null" json:"-"`
+	UserID             uint64               `gorm:"not null" json:"user_id"`
 	User               User                 `gorm:"foreignKey:UserID;references:ID" json:"-"`
 	ModeratorId        uint64               `gorm:"default:null" json:"-"`
 	Moderator          User                 `gorm:"foreignKey:ModeratorId;references:ID" json:"-"`
